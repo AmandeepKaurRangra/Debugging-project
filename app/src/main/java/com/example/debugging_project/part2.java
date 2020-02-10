dcdbde0ebf619b486220b503f022463e348895b4
Sahil Rangra <srangra5455@gmail.com>
2020-02-10 05:44:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/debugging_project/part2.java
@@ -0,0 +1,17 @@
+package com.example.debugging_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class part2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part2);
+        Intent intent=getIntent();
+
+    }
+}
