f2974af06a6b18f86a8d194e2e19742411d8f769
amninderkaursidhu <amaniderkaursidhu@gmail.com>
2020-02-15 09:43:12 -0500

    2nd

--- app/src/main/java/com/example/debugging_project/part2.java
+++ app/src/main/java/com/example/debugging_project/part2.java
@@ -4,14 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class part2 extends AppCompatActivity {
 
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part2);
         Intent intent=getIntent();
+
+        button2=findViewById(R.id.btn2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2=new Intent(part2.this,part3.class);
+                startActivity(intent2);
+            }
+        });
+
 
     }
 }
---
+++ app/src/main/java/com/example/debugging_project/part3.java
@@ -0,0 +1,16 @@
+package com.example.debugging_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class part3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part3);
+        Intent intent2=getIntent();
+    }
+}
